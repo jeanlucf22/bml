628400c3383c04f7710822177575a5e5a68cc294
Michael E. Wall <mewall@lanl.gov>
2020-08-12 14:54:16 -0500

    Modify CMakeLists.txt for build on tulip

---
+++ build_tulip.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Make sure all the paths are correct
+
+module load cmake cray-libsci
+
+rm -r build
+rm -r install
+
+MY_PATH=$(pwd)
+
+export CC=${CC:=gcc}
+export FC=${FC:=gfortran}
+export CXX=${CXX:=g++}
+export BLAS_VENDOR=${BLAS_VENDOR:=Cray}
+export BML_OPENMP=${BML_OPENMP:=yes}
+export INSTALL_DIR=${INSTALL_DIR:="${MY_PATH}/install"}
+export BML_TESTING=${BML_TESTING:=yes}
+export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Debug}
+export EXTRA_CFLAGS=${EXTRA_CFLAGS:=""}
+export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:=""}
+
+./build.sh configure
+
+                                                                                                                                                                                              
+                                    
