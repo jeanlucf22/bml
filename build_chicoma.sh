74f90a28b7a4a88cf620274cc8f8293f8cb07fc9
Mike Wall <mewall@lanl.gov>
2021-02-08 14:42:10 -0700

    Build against openblas that passes all tests

--- build_chicoma.sh
+++ build_chicoma.sh
@@ -12,14 +12,14 @@ MY_PATH=$(pwd)
 export CC=${CC:=cc}
 export FC=${FC:=ftn}
 export CXX=${CXX:=CC}
-export BLAS_VENDOR=${BLAS_VENDOR:=Cray}
+export BLAS_VENDOR=${BLAS_VENDOR:=Auto}
 export BML_OPENMP=${BML_OPENMP:=yes}
 export INSTALL_DIR=${INSTALL_DIR:="${MY_PATH}/install"}
 export BML_TESTING=${BML_TESTING:=yes}
 export CMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Release}
 export EXTRA_CFLAGS=${EXTRA_CFLAGS:="-g"}
-export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:="-g"}
-export CMAKE_Fortran_FLAGS=${CMAKE_Fortran_FLAGS:="-ef"}
+export EXTRA_LINK_FLAGS=${EXTRA_LINK_FLAGS:="-g -L/apps/openblas/0.3.12/lib -lopenblas"}
+export CMAKE_Fortran_FLAGS=${CMAKE_Fortran_FLAGS:="-ef -L/apps/openblas/0.3.12/lib -lopenblas"}
 
 ./build.sh configure
 
---
+++ modules.sh
@@ -0,0 +1,2 @@
+module load gcc openblas
+export LD_LIBRARY_PATH=${CRAY_LD_LIBRARY_PATH}:${LD_LIBRARY_PATH}
